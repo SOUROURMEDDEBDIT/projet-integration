8882974da16cf6432836e34ee1c38366e0ffdc36
sourour <sourour@192.168.1.2>
2014-02-17 07:32:31 +0100

    commit by sourour

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -17,6 +17,10 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        
+        int x;
     }
     
 }
